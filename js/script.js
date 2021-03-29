bd18cd00bd6a0df96e38708e497258c752b7c2b0
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-29 17:45:55 +0200

    Terminato parte genera biglietto

--- js/script.js
+++ js/script.js
@@ -10,15 +10,23 @@ generateButton.addEventListener('click', function() {
     // Leggo il nome inserito dall'utente 
     var nomeUtente = document.getElementById('nome-cognome').value; 
 
+    // Leggo i km che l'utente deve percorrere
+    var kmPercorrere = parseInt( document.getElementById('km-percorrere').value );
+
+    // Stabilisco il prezzo del biglietto
+    var prezzoBiglietto = parseInt( kmPercorrere * 0.21 ) ;
+
     // Leggo l'età dell'utente tramite il valore dell'input
     var etaUtente = document.getElementById('fascia-eta').value;
     // Calcolo l'offerta in base all'età
     var offerta = "Prezzo Standard";
     if ( etaUtente == 'minorenne' ) {
         offerta = 'Sconto Minorenne';
+        prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * 0.2 );
     } else if ( etaUtente == 'over' ) {
         offerta = 'Sconto Silver';
-    }
+        prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * 0.4 );
+    } 
 
     // Stbilisco il numero della carrozza con un numero random
     var carrozza = Math.floor(Math.random() * 10) + 1;
@@ -38,6 +46,9 @@ generateButton.addEventListener('click', function() {
 
     // Codice CP
     document.getElementById('cp-code').innerHTML = cpCode;
+
+    // Prezzo Biglietto
+    document.getElementById('final-price').innerHTML = prezzoBiglietto;
 })
 
 
